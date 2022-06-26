d7096061598e5558d5b9fce1de49defd116d5d96
Lucas Vasconcelos Rocha <61802426+lucasvascrocha@users.noreply.github.com>
2022-06-26 11:42:40 -0300

    up

--- app_jun/App.py
+++ app_jun/App.py
@@ -1,7 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
+ # ----------------------------------LIBS -------------------------------------------------------------   
 import streamlit as st
 st.set_page_config(  # Alternate names: setup_page, page, layout
 	layout="wide",  # Can be "centered" or "wide". In the future also "dashboard", etc.
@@ -22,65 +19,73 @@ import datetime as dt
 dia = dt.datetime.today().strftime(format='20%y-%m-%d')
 
 import style as style
+import home as home
+import login as login
 import pag1 as pag1
 import pag2 as pag2
 import pag3 as pag3
 import pag4 as pag4
 import pag5 as pag5
 
+
+ # ----------------------------------DEFS -------------------------------------------------------------   
+
+#carrega os arquivos css
 def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
-
-
-
-#style.set_background('images/bkgold.png')
-#style.set_css()
-
-    #     st.markdown(
-    # """
-    # <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    # <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    # """,unsafe_allow_html=True
-    #     )
-
-
 def main():
-    
 
- # ----------------------------------NAVBAR -------------------------------------------------------------   
-    
-    n_sprites = option_menu(None,["Análise técnica e fundamentalista", "Comparação de ativos", "Descobrir novos ativos", "Rastreador de trade", "Análise de carteira e previsão de lucro"],
-                        icons=['bar-chart', 'book', 'bullseye', 'binoculars','cash-coin'],
-                         default_index=0, orientation='horizontal',menu_icon="app-indicator",
-                        styles={
-        "container": {"padding": "10!important", "background-color": "#f0eeee" }, # ,"background-size": "cover","margin": "0px"},
-        #"container": {"padding": "0", "background-color": "#fafafa","background-size": "cover","margin": "0px"},
-        #"icon": {"color": "orange", "font-size": "25px"}, 
-        #"nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
-        "nav-link": {"font-size": "16px", "text-align": "center", "--hover-color": "#eee","font-weight": "bold"}, #,"position": "relative","display": "inline"},
-        "nav-link-selected": {"background-color": "#4E90FF"},
-    }
-    ) 
+    #css comum para todas páginas (menu)
+    local_css("style_0.css")  
+
+    #esconder botão de menu e marca dágua no rodapé
+    style.hidden_menu_and_footer()
+
+    #cabeçalho detalhe superior da página 
+    style.headerstyle()
+
+ # ----------------------------------MENU -------------------------------------------------------------   
+
+    with st.sidebar:
+        style.sidebarwidth()    
+        n_sprites = option_menu('Menu',["Home","Login","Análise técnica", "Comparar ações", "Análise fundamentalista", "Rastrear ações", "Previsão de lucro"],
+                            icons=['house','person','bar-chart', 'book', 'bullseye', 'binoculars','cash-coin'],
+                            default_index=0, menu_icon="app-indicator",   #orientation='horizontal',
+                            styles={
+            "container": {"padding": "2!important", "background-color": "#ffffff" }, # ,"background-size": "cover","margin": "0px"},
+            "nav-link": {"font-size": "12px", "text-align": "left", "--hover-color": "#4a7198","font-weight": "bold"}, #,"position": "relative","display": "inline"},
+            "nav-link-selected": {"background-color": "#4a7198"},
+             }) 
 
  # ----------------------------------PAGES -------------------------------------------------------------     
+    if n_sprites == "Home":
+        local_css("style_1.css")      
+        home.initial_page()
+    
+    if n_sprites == "Login":
+        local_css("style_login.css")      
+        login.login_section()
 
-    if n_sprites == "Análise técnica e fundamentalista":
-        #local_css("style_1.css")      
+    if n_sprites == "Análise técnica":
+        local_css("style_1.css")      
         pag1.analise_tecnica_fundamentalista()
 
-    if n_sprites == "Comparação de ativos":
-        #local_css("style_2.css")   
+    if n_sprites == "Comparar ações":
+        local_css("style_2.css")   
         pag2.comparacao_ativos()
 
-    if n_sprites == "Descobrir novos ativos":
+    if n_sprites == "Análise fundamentalista":
+        local_css("style_3.css")   
         pag3.descobrir_ativos()
 
-    if n_sprites == "Rastreador de trade":
+    if n_sprites == "Rastrear ações":
+        local_css("style_4.css")   
         pag4.rastreador()     
 
-    if n_sprites == "Análise de carteira e previsão de lucro":
+    if n_sprites == "Previsão de lucro":
+        local_css("style_5.css")  
         pag5.analise_carteira()
         
 if __name__ == '__main__':
--- app_jun/style.py
+++ app_jun/style.py
@@ -1,6 +1,8 @@
 import base64
 import streamlit as st
 
+from streamlit_option_menu import option_menu
+
 def get_base64(bin_file):
     with open(bin_file, 'rb') as f:
         data = f.read()
@@ -18,83 +20,76 @@ def set_background(png_file):
     ''' % bin_str
     st.markdown(page_bg_img, unsafe_allow_html=True)
 
-def set_css():
-    css_settings ='''
+#esconder botões do streamlit
+def hidden_menu_and_footer():
+    hide_menu = '''
     <style>
-
-    p{
-        color:rgb(0, 0, 0);
-        text-align: center;
-        font-size: 14px;
-        font-family: 'Bree Serif', serif;
+    #MainMenu {
+        visibility:hidden;
     }
 
-    .css-ocqkz7 {
-        display: flex;
-        flex-wrap: wrap;
-        -webkit-box-flex: 1;
-        flex-grow: 1;
-        -webkit-box-align: stretch;
-        align-items: stretch;
-        gap: 1rem;
-        border-radius: 30px;
-        background: rgb(240, 238, 238);
-    }
-
-    .icon {  
-    float: right;
-    font-size:500%;
-    position: absolute;
-    top:0rem;
-    right:-0.3rem;
-    opacity: .16;
-    }
-
-    {
-    width: 800px;
-    display: flex;
-    }
-
-
-    .kpi-card
-    {
-    overflow: hidden;
-    position: relative;
-    box-shadow: 1px 1px 3px rgba(0,0,0,0.75);;
-    display: inline-block;
-    float: left;
-    padding: 1em;
-    border-radius: 0.3em;
-    font-family: sans-serif;  
-    width: 240px;
-    min-width: 180px;
-    margin-left: 0.5em;
-    margin-top: 0.5em;
-    }
-
-    .card-value {
-    display: block;
-    font-size: 200%;  
-    font-weight: bolder;
-    }
-
-    .card-text {
-    display:block;
-    font-size: 70%;
-    padding-left: 0.2em;
-    }
-
-    .card-deck {
-        -webkit-box-orient: horizontal;
-        -webkit-box-direction: normal;
-        -ms-flex-flow: row wrap;
-        flex-flow: row wrap;
-        margin-right: 0px;
-        margin-left: -15px;
-        width: 830px;
-
+    footer{
+        visibility:hidden;
     }
 
     </style>
     '''
-    st.markdown(css_settings, unsafe_allow_html=True)
+    st.markdown(hide_menu, unsafe_allow_html=True)
+
+#linha no cabeçalho branca desing
+def headerstyle():
+    st.markdown(
+    f"""
+    <nav class="navbar fixed-top navbar-light bg-white" style="color: #ffffff; padding: 0.8rem 1rem;">
+        <span class="navbar-brand mb-0 h1" " >  </span>
+    </nav>
+    """, unsafe_allow_html=True
+    )
+
+
+#espaço entre plots
+def space(tamanho):
+    if tamanho == 1:
+        st.title('')
+    if tamanho == 2:
+        st.header('') 
+    if tamanho == 3:
+        st.write('') 
+
+def sidebarwidth():
+    st.markdown(
+        """
+        <style>
+        [data-testid="stSidebar"][aria-expanded="true"] > div:first-child {
+            width: 250px;
+        }
+        [data-testid="stSidebar"][aria-expanded="false"] > div:first-child {
+            width: 250px;
+            margin-left: -500px;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True,
+        )    
+
+def font_google():
+    st.markdown(
+            """
+            <style>
+    @font-face {
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 400;
+    src: url(https://fonts.gstatic.com/s/tangerine/v12/IurY6Y5j_oScZZow4VOxCZZM.woff2) format('woff2');
+    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+    }
+
+        html, body, [class*="css"]  {
+        font-family: 'Roboto';
+        font-size: 48px;
+        }
+        </style>
+
+        """,
+            unsafe_allow_html=True,
+        )
