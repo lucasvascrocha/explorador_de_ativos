5b41451fe190aa643ad570a07150e361b9d8c65c
Lucas Vasconcelos Rocha <61802426+lucasvascrocha@users.noreply.github.com>
2022-06-26 11:44:36 -0300

    up

--- app_jun/App.py
+++ app_jun/App.py
@@ -38,7 +38,7 @@ def local_css(file_name):
 def main():
 
     #css comum para todas páginas (menu)
-    local_css("style_0.css")  
+    #local_css("style_0.css")  
 
     #esconder botão de menu e marca dágua no rodapé
     style.hidden_menu_and_footer()
@@ -61,31 +61,31 @@ def main():
 
  # ----------------------------------PAGES -------------------------------------------------------------     
     if n_sprites == "Home":
-        local_css("style_1.css")      
+        #local_css("style_1.css")      
         home.initial_page()
     
     if n_sprites == "Login":
-        local_css("style_login.css")      
+        #local_css("style_login.css")      
         login.login_section()
 
     if n_sprites == "Análise técnica":
-        local_css("style_1.css")      
+        #local_css("style_1.css")      
         pag1.analise_tecnica_fundamentalista()
 
     if n_sprites == "Comparar ações":
-        local_css("style_2.css")   
+        #local_css("style_2.css")   
         pag2.comparacao_ativos()
 
     if n_sprites == "Análise fundamentalista":
-        local_css("style_3.css")   
+        #local_css("style_3.css")   
         pag3.descobrir_ativos()
 
     if n_sprites == "Rastrear ações":
-        local_css("style_4.css")   
+        #local_css("style_4.css")   
         pag4.rastreador()     
 
     if n_sprites == "Previsão de lucro":
-        local_css("style_5.css")  
+        #local_css("style_5.css")  
         pag5.analise_carteira()
         
 if __name__ == '__main__':
